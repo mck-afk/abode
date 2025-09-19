8b5835963372884d033a0911bae9bca2ec0598ed
annabelle <annabelle@gmail.com>
2025-09-19 17:58:32 +1000

    updated spothecary, added snow, added link to neocities

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-
-
+//Audio Functions
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
 async function playAudio(audioClip) {
@@ -12,3 +10,26 @@ async function playAudio(audioClip) {
 function playSound(audioClip) {
     new Audio(audioClip).play();
 }
+
+// Draggable 
+interact('.draggable')
+  .draggable({
+    listeners: {
+      move: dragMoveListener
+    }
+  })
+
+function dragMoveListener (event) {
+  var target = event.target
+
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
+
+window.dragMoveListener = dragMoveListener
+
